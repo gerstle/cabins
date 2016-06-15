dfa919bbd3bb82d135785e38611b9ddca90f90c7
casey gerstle <gerstle@gmail.com>
2016-06-14 22:29:30 -0700

    lodging display changes.

--- app/helpers/accommodations_helper.rb
+++ app/helpers/accommodations_helper.rb
@@ -1,2 +1,17 @@
 module AccommodationsHelper
+  def conditional_link_to(name = nil, options = nil, html_options = nil, &block)
+    html_options, options, name = options, name, block if block_given?
+    options ||= {}
+
+    html_options = convert_options_to_data_attributes(options, html_options)
+
+    url = url_for(options)
+    html_options['href'] ||= url
+
+    # TODO - check tier, available quantity, etc
+    if (false)
+      content_tag(:a, name || url, html_options, &block)
+    else
+    end
+  end
 end
