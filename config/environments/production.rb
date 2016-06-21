a80a5f075efff4b6753c2eeccdd5148c538d14cb
casey gerstle <gerstle@gmail.com>
2016-06-21 18:03:51 +0000

    setting up gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,10 +66,13 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'info@cabins.inappropirates.com'}
-  config.action_mailer.delivery_method = :sendmail
-  host = 'cabins.inappropirates.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
