7e7368ec4d7c85cc913e348528b9e72a912cb237
casey gerstle <gerstle@gmail.com>
2016-06-20 18:33:32 -0700

    admins should always be able to book, july 13 -> 18

--- app/helpers/accommodations_helper.rb
+++ app/helpers/accommodations_helper.rb
@@ -2,7 +2,7 @@ module AccommodationsHelper
   def can_book(accommodation)
     # make sure user is logged in, has tier defined, and that the tier available has passed
     # make sure accommodation is not already already taken
-    if (!logged_in? || !current_user.tier_id? || current_user.tier.available > DateTime.now || quantity_available?(accommodation) <= 0)
+    if (!logged_in? || (!current_user.tier_id? && !is_admin? ) || (!is_admin? && current_user.tier.available > DateTime.now) || quantity_available?(accommodation) <= 0)
       return false
     end
 
